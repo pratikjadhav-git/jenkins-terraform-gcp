46661fbf30c47beffed80c29cc1da8518b750d8e
Pratik Jadhav <pratikj0077@gmail.com>
2025-12-04 21:48:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "jenkins-terraform-480215"  # <--- REPLACE THIS with your actual Project ID
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "jenkins-created-vm"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      # Ephemeral IP
+    }
+  }
+}
